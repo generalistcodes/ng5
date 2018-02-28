7ced6e559b110c608f5268048abcb0f0e68af8ae
kedomonzter <khemchay@gmail.com>
2018-02-28 18:16:04 +0100

    publishing angular 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+//component decorator
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html', //define html files
+  styleUrls: ['./app.component.scss'] //define styles
 })
+
+//this is all the logic resides
 export class AppComponent {
   title = 'app';
 }
